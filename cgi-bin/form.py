1b9270c581dc4f38a41f900098d12aa01b55f959
Pess-dev <49831290+Pess-dev@users.noreply.github.com>
2022-08-01 20:31:36 +0300

    Update form.py

--- cgi-bin/form.py
+++ cgi-bin/form.py
@@ -1,3 +1,8 @@
+from http.server import HTTPServer, CGIHTTPRequestHandler
+server_address = ("", 8000)
+httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
+httpd.serve_forever()
+
 #!/usr/bin/env python3
 import cgi
 
